e9f3fe152338c5fb4525ebb6b5fa05146b458be7
mkaram538 <mkaram538@tamu.edu>
2025-12-09 17:14:01 -0500

    EV : Adding Static File Management

--- Frontend/Earthquake-Viewer/src/api/earthquakes.ts
+++ Frontend/Earthquake-Viewer/src/api/earthquakes.ts
@@ -19,7 +19,7 @@ interface EarthquakeResponse {
 
 export const fetchEarthquakes = async (): Promise<EarthquakeResponse> => {
   const client = axios.create({
-    baseURL: "http://localhost:8000/api/v1",
+    baseURL: import.meta.env.VITE_BASE_URL,
     timeout: 30000,
     withCredentials: true,
   });
